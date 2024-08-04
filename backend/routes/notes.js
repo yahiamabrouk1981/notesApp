673acd0de75da52be1ca4c6c67b356aca1a2ff36
Yahia Mabrouk <yahiamabrouk1981@gmail.com>
2024-08-04 15:39:17 +0100

    updating the backend

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,39 +1,68 @@
 const express = require("express");
-const Note = require("../models/Note");
+const Note = require("../models/Notes");
 
 const router = express.Router();
 
 // Create a note
 router.post("/notes", async (req, res) => {
-  const note = new Note(req.body);
-  await note.save();
-  res.json(note);
+  try {
+    const note = new Note(req.body);
+    await note.save();
+    res.json(note);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Get all notes
 router.get("/notes", async (req, res) => {
-  const notes = await Note.find();
-  res.json(notes);
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Get a single note
 router.get("/notes/:id", async (req, res) => {
-  const note = await Note.findById(req.params.id);
-  res.json(note);
+  try {
+    const note = await Note.findById(req.params.id);
+    res.json(note);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Update a note
 router.put("/notes/:id", async (req, res) => {
-  const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.json(note);
+  try {
+    const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Delete a note
 router.delete("/notes/:id", async (req, res) => {
-  await Note.findByIdAndDelete(req.params.id);
-  res.sendStatus(204);
+  try {
+    await Note.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//Delete all notes
+router.delete("/notes", async (req, res) => {
+  try {
+    await Note.deleteMany({});
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/notes-app", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://yahiamabrouk1981:lmD3ShPCAug2FqSY@cluster0.xg8hgn2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+console.log("Connected to DB");
 
 app.use("/api", noteRoutes);
 
